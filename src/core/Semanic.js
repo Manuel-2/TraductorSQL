449dbe6050a32439a9834d3c278b53c4d9d375c7
Manu <25lopezm25@gmail.com>
2026-05-05 21:13:22 -0700

    pk y fk deben existir en la tabla

--- src/core/Semanic.js
+++ src/core/Semanic.js
@@ -17,8 +17,30 @@ export class Semantic {
       700: (token) => { this.#addTable(token) },
       701: (token) => { this.#addAtribute(token) },
       702: (token) => { this.#checkDataType(token) },
+      703: (token) => { this.#keyAtrExist(token) },
     };
   };
+
+  // UTILS
+
+  /**
+   * Obten la informacion de un trabito en una tabla especifica
+   * @param {string} atrId talba.atributo 
+   */
+  getAtr(atrId) {
+    return this.atributes[atrId] ?? false;
+  }
+
+  /**
+   * recive un nombre de atributo y retorna su informacion en al tabla actual
+   * @param {string} atrName 
+   * @returns tablaActual.atrName
+   *
+  */
+  getAtrCT(atrName) {
+    return this.getAtr(this.currentTable.name + "." + atrName);
+  }
+
 
   // Routines ------------------------------------
   #addTable(token) {
@@ -41,10 +63,10 @@ export class Semantic {
     let currentTableName = this.currentTable.name;
     let atrId = currentTableName + "." + name;
 
-    if (this.atributes[atrId]?.table == this.currentTable) {
+    if (this.atributes[atrId] != undefined) {
       Sql.error({
         code: 302,
-        message: `Nombre de atributo “${name}” está duplicado en la tabla: "${this.currentTable.name}"`
+        message: `Nombre de atributo: “${name}” está duplicado en la tabla: "${this.currentTable.name}"`
       },
         token.line
       );
@@ -72,4 +94,16 @@ export class Semantic {
         token.line
       );
   }
+
+  #keyAtrExist(token) {
+    if (this.getAtrCT(token.tok) == false) {
+      Sql.error({
+        code: 303,
+        message: `El nombre del atributo(llave): “${token.tok}" no existe en la tabla: “${this.currentTable.name}”.`
+      },
+        token.line
+      );
+    }
+  }
+
 }
